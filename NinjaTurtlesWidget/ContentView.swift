c4bfde8478d44dc61a43e1c37309582d8ce5343e
Ünsal Öner <unsal.oner@hotmail.com>
2022-03-26 20:15:08 +0300

    Initial Commit

---
+++ NinjaTurtlesWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NinjaTurtlesWidget
+//
+//  Created by Unsal Oner on 26.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NinjaTurtlesWidget/NinjaTurtlesWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NinjaTurtlesWidgetApp.swift
+//  NinjaTurtlesWidget
+//
+//  Created by Unsal Oner on 26.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct NinjaTurtlesWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
